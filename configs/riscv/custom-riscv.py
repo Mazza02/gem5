38d1d3f50a802f64160fa4dba037f380c0fd0921
Mazza02 <2551634s@student.gla.ac.uk>
2025-12-18 12:43:29 +0000

    custom config hotfix

--- configs/riscv/custom-riscv.py
+++ configs/riscv/custom-riscv.py
@@ -26,19 +26,6 @@
 
 import m5
 from m5.objects import *
-from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
-    MESITwoLevelCacheHierarchy,
-)
-
-cache_hierarchy = MESITwoLevelCacheHierarchy(
-    l1d_size="16KiB",
-    l1d_assoc=8,
-    l1i_size="16KiB",
-    l1i_assoc=8,
-    l2_size="256KiB",
-    l2_assoc=16,
-    num_l2_banks=1,
-)
 
 system = System()
 
